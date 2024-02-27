5f61fb9417ed317deaa561ef7179b1fe4b94bf8d
LAB02-PC15 <LAB02-PC15@DESKTOP-L7LEES6>
2024-02-26 21:48:27 -0300

    Primeiro commit

---
+++ Aula_4/src/Model/Conta.java
@@ -0,0 +1,11 @@
+package Model;
+
+public class Conta {
+      protected double saldo;
+      protected String email;
+      
+      Conta(String email) {
+    	  this.saldo = 0;
+    	  this.email = email;
+      }
+}
---
+++ Aula_4/src/Model/ContaCorrente.java
@@ -0,0 +1,37 @@
+package Model;
+
+public class ContaCorrente extends Conta{
+	private double ValorTaxa;
+	
+	ContaCorrente(String email) {
+		super(email);
+		this.ValorTaxa = ValorTaxa;
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String Saque(double valorSaque) {
+		if(valorSaque + ValorTaxa <= saldo) {
+		saldo =- ValorTaxa;
+		saldo =- valorSaque;
+		return (
+			"Valor Sacado: " + valorSaque
+			+"\nValor Saldo Após o Saque: " + 
+		    saldo
+		    );
+	     } else {
+	    	 return "Saldo insuficiente";
+	     }
+	}
+
+	public String Deposito(double valorDeposito) {
+		saldo += valorDeposito;
+		return ("Valor Sacado: " + valorDeposito
+				+"\nValor Saldo Após o Saque: "
+			    + saldo);
+	}
+	
+	public double getSaldo() {
+		return saldo;
+	}
+	
+}
